08d1f08f9e96efac976da3686fc63b181bddf3e3
fable-facet <guardian@fablefacet.com>
2026-06-04 10:13:22 -0300

    Update index.js

--- 69428882/1090662013/index.js
+++ 69428882/1090662013/index.js
@@ -21,13 +21,6 @@
         "html_url": "https://github.com/casson-projects-2020/untainted-heart/blob/main/description.md"
     },
     {
-        "name": "sand_crawler.jpeg",
-        "path": "sand_crawler.jpeg",
-        "sha": "44af5a06e23179bacb3cdbf7bb7b9f4f76669fc0",
-        "size": 142524,
-        "html_url": "https://github.com/casson-projects-2020/untainted-heart/blob/main/sand_crawler.jpeg"
-    },
-    {
         "name": "sand_crawler.jpg",
         "path": "sand_crawler.jpg",
         "sha": "fba0e4e392cb470569b5f5d0c2d81353948bcd79",
